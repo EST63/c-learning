728464d90e1c68af9bdd271e19837ef2769e3d49
evgeny <etakoito@yandex.ru>
2026-03-08 10:41:44 +0300

    fix - age_calculator

--- exercises/age_calculator.c
+++ exercises/age_calculator.c
@@ -1,22 +1,32 @@
-// age_calculator.c - каалькулятор возраста с улудшениями
+// age_calculator.c - калькулятор возраста с улудшениями
 #include <stdio.h>
+#include <string.h>
+
 int main(void)
 {
     // запрос имени с помощью fgets()
     printf("Введите ваше имя: ");
     char name[50];
     fgets(name, sizeof(name), stdin);
-    // запрос года рождения с помощью scanf()
-    int age;
-    printf("Введите ваш год рождения: ");
-    if ((scanf("%d", &age)) != 1)
+    // удаление последнего символа - перевод строки
+    size_t len = strlen(name);
+    if (len > 0 && name[len - 1] == '\n')
     {
-        printf("Вы ввели некорректные данные\n");
+        name[len - 1] = '\0';
+    }
+
+    // запрос года рождения с помощью scanf()
+    int brith_year;
+    printf("Введите ваш год рождения: ");
+    if ((scanf("%d", &brith_year)) != 1)
+    {
+        printf("Ошибка введите целое число.\n");
         return 1;
     }
-    if (age <= 0)
+    // проверка на разумный год
+    if (brith_year < 1900 || brith_year > 2026)
     {
-        printf("Вы ввели некорректный год рождения\n");
+        printf("Ошибка:год рождения вне диапозона от 1900 до 2026\n");
         return 1;
     }
     while (getchar() != '\n')
@@ -31,9 +41,9 @@ int main(void)
         printf("Вы ввели некорректные данные\n");
         return 1;
     }
-    if (current_year <= 0)
+    if (current_year < 2025 || current_year > 2026)
     {
-        printf("Вы ввели некорректный год\n");
+        printf("Ошибка: текущий год вне диапозона от 2025 до 2026\n");
         return 1;
     }
     while (getchar() != '\n')
@@ -41,15 +51,17 @@ int main(void)
     }
 
     // расчет возраста
-    int year_of_birth = current_year - age;
+    int age = current_year - brith_year;
     // если возраст вне диапозона от 0 до 150 - сообщение об ошибке и завершение программы
-    if (year_of_birth < 0 || year_of_birth > 150)
+    if (age < 0 || age > 150)
     {
         printf("Ваш возраст вне диапозона от 0 до 150\n");
         return 1;
     }
-    printf("Ваше имя: %s", name);
-    printf("Ваш возраст: %d\n", year_of_birth);
-    printf("Ваш год рождения: %d\n", age);
+
+    // вывод результатов
+    printf("Ваше имя: %s\n", name);
+    printf("Ваш возраст: %d\n", age);
+    printf("Ваш год рождения: %d\n", brith_year);
     return 0;
 }
